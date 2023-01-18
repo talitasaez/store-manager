27ea7b720c55c1525b5a91316eb995075c67a237
talitasaez <tltsaez@gmail.com>
2023-01-18 18:10:16 -0300

    testes model ok

---
+++ tests/unit/models/productsModels.test.js
@@ -0,0 +1,40 @@
+const { chai } = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+const productsModel = require('../../../src/models/productsModel');
+const connection = require('../../../src/models/connection');
+const productsMock = require('../mocks/productsMock.test');
+
+// const { expect } = chai;
+// chai.use(sinonChai);
+
+
+describe('Model de produtos', function () {
+  describe('Listar todos os produtos', function () {
+
+    // beforeEach(() => {
+    //   sinon.stub(connection, 'execute').resolves([productsMock]);
+    // });
+
+    it('Deve retornar um array com todos os elementos', async function () {
+      sinon.stub(connection, 'execute').resolves([productsMock]);
+
+      const result = await productsModel.getAllProducts();
+
+      expect(result).to.be.a('array');
+    });
+
+    it('Deve retornar produto por id ', async function () {
+      sinon.stub(connection, 'execute').resolves([[productsMock[0]]]);
+
+      const result = await productsModel.getProductsById(1);
+
+      expect(result).to.be.deep.equal(productsMock[0]);
+    });
+    afterEach(() => {
+      sinon.restore();
+   })
+
+  })
+
+});
